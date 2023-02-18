bdce2c1515e2c7fa50cfed5017279f7e33892b78
BrayanSosaAlberto <125327983+BrayanSosaAlberto@users.noreply.github.com>
2023-02-17 20:15:12 -0600

    ProyectoAsincronaSemana4

--- AsincronaClase4/Program.cs
+++ AsincronaClase4/Program.cs
@@ -1,4 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using tarea4.entidades;
+using tarea4.negocio;
 
-Console.WriteLine("ClaseAsincrona");
+EntidadContador Texto = new EntidadContador ();
+ClsNegocioContador clsNegocioContador = new ClsNegocioContador();
+
+
+
+  Console.WriteLine("Ingrese un Texto:");
+  string textoIngresado = Console.ReadLine();
+
+  EntidadContador entidad = new EntidadContador();
+  entidad.Texto = textoIngresado;
+
+  ClsNegocioContador negocio = new ClsNegocioContador();
+  int cantidadLetras = negocio.ContarLetras(entidad);
+
+  Console.WriteLine ("La cantidad de letras ingresadas es: " + cantidadLetras);
